fc1c6665b4a06ebc03b3d35cff6d418c65d6c78d
Taiga Kitao <taiga20022002@gmail.com>
2021-10-11 12:29:42 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by 北尾　大河 on 2021/10/11.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
